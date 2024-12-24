580a833b97224638b3c180eab9ad6016bb73b1df
Aji T A <ajiajith001@gmail.com>
2024-12-24 12:46:53 +0530

    CatalogAPI - implement ICommand , ICommandHander in CreateProduct feature handler

--- src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
@@ -1,4 +1,5 @@
-﻿using MediatR;
+﻿using BuildingBlocks.CQRS;
+using MediatR;
 
 namespace CatalogAPI.Products.CreateProduct;
 
@@ -8,12 +9,12 @@ public record CreateProductCommand(
     string Description,
     string ImageFile,
     decimal Price
-): IRequest<CreateProductResult>;
+): ICommand<CreateProductResult>;
 public record CreateProductResult(Guid Id);
 
-internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
-    public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    public Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
         // Business logic to create a product
         throw new NotImplementedException();
