95c1c1707dcb225ae7216c8101a5e59bf7c81ac3
Aji T A <ajiajith001@gmail.com>
2025-01-20 21:17:37 +0530

    BasketAPI - configuring Carter, MediaR and  dummy endpoints

---
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketEndpoints.cs
@@ -0,0 +1,22 @@
+﻿namespace Basket.API.Basket.GetBasket
+{
+    //public record GetBasketRequest(string UserName);
+    public record GetBasketResponse(ShoppingCart Cart);
+    public class GetBasketEndpoints : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/basket/{username}", async (string username, ISender sender) =>
+            {
+                var result = await sender.Send(new GetBasketQuery(username));
+                var response = result.Adapt<GetBasketResponse>();
+                return Results.Ok(response);
+            })
+            .WithName("GetBasketByUsername")
+            .Produces<GetBasketResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Basket details by username")
+            .WithDescription("Get Basket details by username");
+        }
+    }
+}
--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,9 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add service to the container
+var assembly = typeof(Program).Assembly;
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssemblies(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LogginBehavior<,>));
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
+app.MapCarter();
 
 app.Run();
