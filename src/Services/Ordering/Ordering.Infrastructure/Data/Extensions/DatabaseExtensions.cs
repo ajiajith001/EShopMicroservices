aead00e23fcf456609ad988240e1772d97942117
Aji T A <ajiajith001@gmail.com>
2025-02-21 17:52:18 +0530

    Ordering.Infrastructure - Seed Order data

--- src/Services/Ordering/Ordering.Infrastructure/Data/Extensions/DatabaseExtensions.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Extensions/DatabaseExtensions.cs
@@ -11,5 +11,41 @@ public static class DatabaseExtensions
         var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
 
         context.Database.MigrateAsync().GetAwaiter().GetResult();
+
+        await SeedAsync(context);
+    }
+
+    private static async Task SeedAsync(AppDbContext context)
+    {
+        await SeedCustomerAsync(context);
+        await SeedProductAsync(context);
+        await SeedOrderAndItemsAsync(context);
+    }
+
+    private static async Task SeedCustomerAsync(AppDbContext context)
+    {
+        if (!await context.Customers.AnyAsync())
+        {
+            await context.Customers.AddRangeAsync(InitialData.Customers);
+            await context.SaveChangesAsync();
+        }
+    }
+
+    private static async Task SeedProductAsync(AppDbContext context)
+    {
+        if (!await context.Products.AnyAsync())
+        {
+            await context.Products.AddRangeAsync(InitialData.Products);
+            await context.SaveChangesAsync();
+        }
+    }
+
+    private static async Task SeedOrderAndItemsAsync(AppDbContext context)
+    {
+        if (!await context.Orders.AnyAsync())
+        {
+            await context.Orders.AddRangeAsync(InitialData.OrdersWithItems);
+            await context.SaveChangesAsync();
+        }
     }
 }
